e243fdc9ac2cca878c13edc8acb248b225780d98
Yu-Ting <67809413+ytchang05@users.noreply.github.com>
2026-05-08 06:59:39 -0400

    allow fixing params in makegp_fourier

--- src/discovery/signals.py
+++ src/discovery/signals.py
@@ -471,7 +471,7 @@ make_dmfourierbasis = make_fourierbasis_dm  # backward-compatible alias
 def make_fourierbasis_chrom(alpha=4.0, tndm=False):
     return make_fourierbasis_dm(alpha=alpha, tndm=tndm)
 
-def makegp_fourier(psr, prior, components, T=None, mean=None, fourierbasis=fourierbasis, common=[], exclude=['f', 'df'], name='fourierGP'):
+def makegp_fourier(psr, prior, components, T=None, mean=None, fourierbasis=fourierbasis, common=[], exclude=['f', 'df'], name='fourierGP', noisedict={}):
     argspec = inspect.getfullargspec(prior)
     argmap = [(arg if arg in common else f'{name}_{arg}' if f'{name}_{arg}' in common else f'{psr.name}_{name}_{arg}') +
               (f'({components[arg] if isinstance(components, dict) else components})' if argspec.annotations.get(arg) == typing.Sequence else '')
@@ -498,11 +498,14 @@ def makegp_fourier(psr, prior, components, T=None, mean=None, fourierbasis=fouri
         def fmatfunc(params):
             return fmat(*[params[arg] for arg in fargmap])
         fmatfunc.params = fargmap
-
-    gp = matrix.VariableGP(matrix.NoiseMatrix12D_var(priorfunc), fmatfunc if callable(fmat) else fmat)
-    gp.index = {f'{psr.name}_{name}_coefficients({len(f)})': slice(0,len(f))} # better for cosine
-    gp.name, gp.pos = psr.name, psr.pos
-    gp.gpname, gp.gpcommon = name, common
+        
+    if all(arg in noisedict for arg in argmap):
+        return matrix.ConstantGP(matrix.NoiseMatrix1D_novar(priorfunc(noisedict)), fmat)
+    else:
+        gp = matrix.VariableGP(matrix.NoiseMatrix12D_var(priorfunc), fmatfunc if callable(fmat) else fmat)
+        gp.index = {f'{psr.name}_{name}_coefficients({len(f)})': slice(0,len(f))} # better for cosine
+        gp.name, gp.pos = psr.name, psr.pos
+        gp.gpname, gp.gpcommon = name, common
 
     if mean is not None:
         margspec = inspect.getfullargspec(mean)
